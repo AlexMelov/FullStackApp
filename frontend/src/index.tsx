3c61e44333330b250591297f0fb70f43c0e29caa
redaxmedia <info@redaxmedia.com>
2022-03-10 16:59:13 +0100

    Add enviroment files to frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { environment } from './environments/environment.js';
+import environment from './environments/environment.js';
 
 ReactDOM.render(
   <React.StrictMode>
