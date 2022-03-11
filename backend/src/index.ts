42a8059fa7a2a73b4616ca8dde72982337380625
Aleksandar <melov.aleksandar2@gmail.com>
2022-03-11 09:40:00 +0100

    fixed connection error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,18 @@
 import express, { Express, Request, Response } from 'express';
 import mongoose from 'mongoose';
+
+import('dotenv/config');
+
 import bodyParser from 'body-parser';
 
 const server: Express = express();
 
 server.use(bodyParser.json());
-server.get('/', (request: Request, response: Response) => response.send('NOTHING HERE'));
-mongoose.connect(process.env.DB_URL, () => console.log('CONNECTED TO DATABASE'));
+server.get('/', (request: Request, response: Response) =>
+	response.send('NOTHING HERE')
+);
+
+mongoose.connect(process.env.DB_URL, () =>
+	console.log('CONNECTED TO DATABASE')
+);
 server.listen(8000);
