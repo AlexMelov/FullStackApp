4787ed37d8a9055f2d2f711a01251f1a7acf484a
Aleksandar <melov.aleksandar2@gmail.com>
2022-03-15 16:33:05 +0100

    changed todo from cypress

--- frontend/cypress/integration/todo-test.js
+++ frontend/cypress/integration/todo-test.js
@@ -1,9 +1,11 @@
-describe ('todo',() =>{
+describe ('todo',() => {
 	it('user can add todo', ()=>{
 		// add todoItem
 		// delete todoItem
 		cy.visit('localhost:3000')
 		cy.findByRole('button', {  name: /add todo/i}).click()
-		cy.findByRole('textbox', {  name: /todo text/i}).type('My todo')
+		cy.findByRole('textbox', {  name: /todo text/i}).type('My new todo!!!')
+		let oldTodo;
+		cy.get('.List_todos__8LYu9 > :nth-child(6)').then($title=>oldTodo=$title.text('Changed Todo from Cypress'))
 	})
 })
