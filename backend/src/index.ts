9f5e16306785468d9bc5b2c334a5bfaf964e988e
Aleksandar <melov.aleksandar2@gmail.com>
2022-03-30 10:46:21 +0200

    setted hardcoded routes instead env variables

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,22 +3,22 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import { deleteHandler, getHandler, postHandler } from './controller.js';
 import cors from 'cors';
-import environment from '../environments/environment.dev.js';
+// import environment from '../environments/environment.dev.js';
 
 const server : Express = express();
 
 server.use(cors());
 server.use(bodyParser.json());
-server.get(`${environment.apiUrl}:8000`, (request : Request, response : Response) => response.sendStatus(404));
-server.get(environment.apiRoutes.api + environment.apiRoutes.todos, getHandler);
-server.post(environment.apiRoutes.api + environment.apiRoutes.todos, postHandler);
-server.delete(environment.apiRoutes.api + environment.apiRoutes.todosWithId, deleteHandler);
+server.get('/', (request : Request, response : Response) => response.sendStatus(404));
+server.get('/api/todos', getHandler);
+server.post('/api/todos', postHandler);
+server.delete('/api/todos/:todoId', deleteHandler);
 
 mongoose.connect(process.env.DB_URL)
 	.then(() =>
 	{
 		process.stdout.write('CONNECTION TO DATABASED SUCCEED');
-		server.listen(process.env.PORT || environment.apiPort);
+		server.listen(8000);
 	})
 	.catch(() =>
 	{
