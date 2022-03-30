f91495a5e58ce3619f5d966ccca7a6609f728ab3
Aleksandar <melov.aleksandar2@gmail.com>
2022-03-30 10:24:46 +0200

    setted routes only for backend

--- backend/src/api/routes.ts
+++
@@ -1,5 +0,0 @@
-import environment from '../../environments/environment.dev';
-import { Request, Response } from 'express';
-import server from '../index';
-
-server.get(`${environment.apiUrl}:`, (request : Request, response : Response) => response.sendStatus(404));
