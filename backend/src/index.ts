a325795577c0d0475fbacd1e2ade7c1d8fb06eeb
Aleksandar <melov.aleksandar2@gmail.com>
2022-03-30 17:05:19 +0200

    uppercase PORT

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,7 @@ mongoose.connect(process.env.DB_URL)
 	.then(() =>
 	{
 		process.stdout.write('CONNECTION TO DATABASED SUCCEED');
-		server.listen(process.env.Port || environment.apiPort);
+		server.listen(process.env.PORT || environment.apiPort);
 	})
 	.catch(() =>
 	{
