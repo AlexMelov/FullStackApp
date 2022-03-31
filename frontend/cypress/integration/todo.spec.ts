751ba427820e1bf83928f8737fd3c3e31200f586
Aleksandar <melov.aleksandar2@gmail.com>
2022-03-31 12:25:21 +0200

    changed cypress url to env variable

--- frontend/cypress/integration/todo.spec.ts
+++ frontend/cypress/integration/todo.spec.ts
@@ -1,8 +1,10 @@
+import environment from '../../src/environments/environment';
+
 describe('todo', () =>
 {
 	beforeEach(() =>
 	{
-		cy.visit('localhost:3000');
+		cy.visit(environment.apiUrl);
 	});
 
 	it('todo list is working', () =>
