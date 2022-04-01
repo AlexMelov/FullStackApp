536541ef156a1e32d5fdb12348287d6787de0abb
Aleksandar Melov <80846978+AlexMelov@users.noreply.github.com>
2022-04-01 16:54:32 +0200

    Develop (#20)
    * fixed yml file and env, cypress working with dev also
    
    * stage phase work locally, added ci-stage.yml and set the variables
    
    * set wait for serve on test-smoke
    
    * set wait for serve removed on test-smoke
    
    * uniformed secrets
    
    * changed cypress code because of test-smoke fail
    
    * changed baseUrl in env file stage, also check the linter for backend models

--- frontend/cypress/integration/todo.spec.ts
+++ frontend/cypress/integration/todo.spec.ts
@@ -12,12 +12,15 @@ describe('todo', () =>
 
 	it('todo list is working', () =>
 	{
+		cy.wait(2000);
+		cy.reload();
 		cy.get('[data-test="item"]').should('have.length.above', 0);
 		cy.get('[data-test="item"]').should('have.length.below', 30);
 	});
 
 	it('adding todo to list', () =>
 	{
+		cy.reload();
 		cy.get('[data-test="item"]').last().should('not.contain.text', 'My new Todo from Cypress');
 		cy.get('[data-test="text-input"]').type('My new Todo from Cypress');
 		cy.get('[data-test="add-button"]').click();
--- frontend/src/environments/environment.stage.ts
+++ frontend/src/environments/environment.stage.ts
@@ -7,12 +7,12 @@ export const environment : Environment =
 		branch: 'master',
 		environment: 'stage'
 	},
-	baseUrl : 'http://localhost:3000',
-	apiUrl: 'https://full-stack-a3sgu4q5h-alexmelov.vercel.app',
+	baseUrl : 'https://todos-stage.vercel.app',
+	apiUrl: 'https://todos-stage.vercel.app/api',
 	apiRoutes:
 	{
-		todos: '/api/todos',
-		todosWithId: '/api/todos/:todoId'
+		todos: '/todos',
+		todosWithId: '/todos/:todoId'
 	}
 };
 
