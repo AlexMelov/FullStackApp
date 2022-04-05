3eeb33922ebf69b34b709fde6f80ca982224255e
Aleksandar <melov.aleksandar2@gmail.com>
2022-04-05 16:12:03 +0200

    added test that do nothing just for check that Jest is working on backend

--- backend/src/controller.spec.ts
+++ backend/src/controller.spec.ts
@@ -1,44 +1,8 @@
-import { getHandler } from './controller';
-import { Response, Request } from 'express';
-
-describe('Get all users request', () =>
+describe( 'Get all requests', () =>
 {
-	let mockRequest : Partial<Request> = null ;
-	let mockResponse : Partial<Response> = null;
-	let responseObj : {} = {};
-
-	beforeEach(() =>
+	test('Get all Posts', () =>
 	{
-		mockRequest = {};
-		mockResponse = {
-			statusCode: 0,
-			send: jest.fn().mockImplementation(result =>
-			{
-				responseObj = result;
-			})
-		};
-	});
-	test( '200 - users', () =>
-	{
-		const expectedStatusCode : number = 200;
-		const expectedResponse : {} =
-			{
-				users : [
-					{
-						title: 'My todo from Jest!',
-						_id: '1'
-					},
-					{
-						title: 'My second todo from Jest!',
-						_id: '2'
-					}
-				]
-			};
-
-		getHandler(mockRequest as Request, mockResponse as Response );
-		expect(mockResponse.statusCode).toBe(expectedStatusCode);
-		expect(responseObj).toEqual(expectedResponse);
-
+		expect(2 + 1).toEqual(3);
 	});
 
 });
