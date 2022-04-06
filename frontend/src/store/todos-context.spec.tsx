efc372a4280450ad90850b0424ec58985684897b
Aleksandar <melov.aleksandar2@gmail.com>
2022-04-06 16:43:27 +0200

    run some more jest tests on frontend

--- frontend/src/store/todos-context.spec.tsx
+++ frontend/src/store/todos-context.spec.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import TodosContextProvider, { TodosContext } from './todos-context';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { fireEvent, render } from '@testing-library/react';
 
 describe('TodoProvider', ()=>
 {
@@ -32,5 +33,45 @@ describe('TodoProvider', ()=>
 		wrapper.find('button').simulate('click');
 
 		expect(wrapper.find('[data-testing="title"]').text()).toBe('Test Todo!');
+	});
+
+	it('remove todo', () =>
+	{
+		const { getByText } = render(
+			<TodosContextProvider>
+				<TodosContext.Consumer>
+					{
+						value => <React.Fragment>
+							<button onClick={() => value.removeTodo(1)}>Remove Todo</button>
+						</React.Fragment>
+					}
+				</TodosContext.Consumer>
+			</TodosContextProvider>);
+
+		fireEvent.click(getByText('Remove Todo'));
+	});
+	it('add todo', () =>
+	{
+		const { getByText } = render(
+			<TodosContextProvider>
+				<TodosContext.Consumer>
+					{
+						value => <React.Fragment>
+							<button onClick={() => value.addTodo('New todo From Jest!')}>Add Todo</button>
+							<ul>
+								{value.items.map(item =>
+								{
+									if(item.title === 'New todo From Jest!')
+									{
+										<li>{item.title}</li>;
+									}
+								})}
+							</ul>
+						</React.Fragment>
+					}
+				</TodosContext.Consumer>
+			</TodosContextProvider>);
+
+		fireEvent.click(getByText('Add Todo'));
 	});
 });
