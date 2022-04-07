0653a322a98e9ec5fc29af459e0aa75ec57eac4f
Aleksandar <melov.aleksandar2@gmail.com>
2022-04-07 14:04:50 +0200

    sending post request done in backend and get the todoID

--- backend/src/server.spec.ts
+++ backend/src/server.spec.ts
@@ -2,19 +2,30 @@ import supertest, { Response } from 'supertest';
 import mongoose from 'mongoose';
 import { server } from './server.js';
 
+let todoId : string;
+
 describe( 'Server', () =>
 {
-	beforeAll(done => 
-	{
-		done();
-	});
 	it('GET /todos', async() =>
 	{
 		const response : Response = await supertest(server).get('/todos');
 
 		expect(response.statusCode).toBe(200);
 	});
-	afterAll(done => 
+
+	it('POST /todos', async() =>
+	{
+		const response : Response = await supertest(server).post('/todos')
+			.send({ title : 'Todo From Jest!' }).expect(200);
+		const sendedData : {title : string, _id : string} = await response.body;
+
+		todoId = await sendedData._id;
+		expect(response.headers).toBeDefined();
+		expect(sendedData.title).toContain('Todo From Jest!');
+		expect(sendedData._id).toContain(todoId);
+	});
+
+	afterAll(done =>
 	{
 		mongoose.connection.close();
 		done();
