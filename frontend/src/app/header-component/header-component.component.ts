e0600100766dd20d524e3223546df3da3dee4180
Aleksandar <melov.aleksandar2@gmail.com>
2022-04-12 10:29:00 +0200

    ngFor, ngIf and style done

--- frontend/src/app/header-component/header-component.component.ts
+++ frontend/src/app/header-component/header-component.component.ts
@@ -7,11 +7,48 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponentComponent
 {
+	items : Array<{title : string, _id : number}> =
+		[
+      {
+        title:'First todo',
+        _id: 1
+      },
+      {
+        title:'Second todo',
+        _id: 2
+      },
+      {
+        title:'Third todo',
+        _id: 3
+      },
+      {
+        title:'Fourth todo',
+        _id: 4
+      },
+      {
+        title:'Fifth todo',
+        _id: 5
+      }
+
+		];
 	listItem : string = 'Item1';
 	new_item : string = '';
-
+	status_item : string = '';
+	list_item_created : boolean = false;
+	constructor()
+	{
+	  this.status_item = Math.random() > 0.5 ? 'Checked' : 'Not Checked';
+	}
 	update_item_name(event : KeyboardEvent) : void
 	{
     this.new_item = (<HTMLInputElement>event.target).value;
+	}
+	add_text() : void
+	{
+    this.list_item_created = true;
+	}
+	change_color() : void
+	{
+		this.status_item === 'Checked' ? 'green' : 'red';
 	}
 }
