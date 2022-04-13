06c5de4103ef4049e0a4c9fa9b956c0d5612b88d
Aleksandar <melov.aleksandar2@gmail.com>
2022-04-13 11:23:37 +0200

    run cypress in headless mode and run empty test

--- frontend/cypress/integration/cypress-test.spec.ts
+++ frontend/cypress/integration/cypress-test.spec.ts
@@ -2,6 +2,12 @@ describe('run empty test', () =>
 {
   it('should run empty test', () =>
   {
-    expect(true).equals(true)
+    cy.visit('localhost:4200')
+    expect( true ).to.equal(true)
+  })
+  it('should run another empty test', () =>
+  {
+    expect( 1 + 2 ).to.equal(3)
+    Cypress.config('execTimeout', 3000)
   })
 })
