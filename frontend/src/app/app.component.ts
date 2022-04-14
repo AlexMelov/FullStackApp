8deba7c8135106237d9f6437fb99abf4ff584134
Aleksandar <melov.aleksandar2@gmail.com>
2022-04-14 16:21:02 +0200

    solving PR issues

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent
+export class AppComponent implements OnInit
 {
 	title : string = 'frontend';
+	todos_array : any[] = [];
+	todos : { title : string, _id : string }[] = [];
+	constructor(private http : HttpClient)
+	{
+	}
+	ngOnInit() : void
+	{
+		this.fetchItems();
+	}
+	 fetchItems() : void
+	{
+		this.http.get(environment.apiUrl + environment.apiRoutes.todos)
+			.subscribe(data => this.todos_array.push(data));
+		this.todos = this.todos_array[0];
+	}
 }
