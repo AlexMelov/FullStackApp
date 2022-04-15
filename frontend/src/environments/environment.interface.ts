8a2d398c833297194bfb7728c2fbc7ed0d8913aa
Aleksandar <melov.aleksandar2@gmail.com>
2022-04-15 11:58:39 +0200

    corrected indent on env.interface

--- frontend/src/environments/environment.interface.ts
+++ frontend/src/environments/environment.interface.ts
@@ -2,14 +2,14 @@ export interface Environment
 {
 	metadata :
 	{
-	branch : string;
-	environment : string
+		branch : string;
+		environment : string
 	};
 	baseUrl : string;
 	apiUrl : string;
 	apiRoutes :
 	{
-	todos : string;
-	todosWithId : string;
-	};
+		todos : string;
+		todosWithId : string;
+	}
 }
