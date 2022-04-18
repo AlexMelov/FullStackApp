d7fe4e3c229f5e994e320b2c27050bc24810006f
Aleksandar <melov.aleksandar2@gmail.com>
2022-04-18 15:11:42 +0200

    set indent on list component

--- frontend/src/app/list/list.component.ts
+++ frontend/src/app/list/list.component.ts
@@ -2,14 +2,11 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Todo } from '../todo.intercace';
 
 @Component(
-	{
-		selector: 'app-list',
-		templateUrl: './list.component.html',
-		styleUrls:
-			[
-				'./list.component.scss'
-			]
-	})
+{
+	selector: 'app-list',
+	templateUrl: './list.component.html',
+	styleUrls:[ './list.component.scss' ]
+})
 export class ListComponent
 {
 	@Input() todo : Todo = {};
