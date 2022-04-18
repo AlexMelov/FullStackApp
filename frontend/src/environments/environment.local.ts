dc86e69007866b7d9450364d4bcf2be5a3eeafac
Aleksandar <melov.aleksandar2@gmail.com>
2022-04-18 15:24:37 +0200

    set indent on environment.local.ts

--- frontend/src/environments/environment.local.ts
+++ frontend/src/environments/environment.local.ts
@@ -3,16 +3,16 @@ import { Environment } from './environment.interface';
 export const environment : Environment =
 {
 	metadata:
-		{
-			branch: 'develop',
-			environment: 'local'
-		},
+	{
+		branch: 'develop',
+		environment: 'local'
+	},
 	baseUrl : 'http://localhost:4200',
 	apiUrl: 'http://localhost:8000',
 	apiRoutes:
-		{
-			todos: '/todos',
-			todosWithId: '/todos/:todoId'
-		}
+	{
+		todos: '/todos',
+		todosWithId: '/todos/:todoId'
+	}
 };
 
