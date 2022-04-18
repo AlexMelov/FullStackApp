52329296f309730ff219cf0de7afa98ddf236499
Aleksandar <melov.aleksandar2@gmail.com>
2022-04-18 15:12:45 +0200

    remove fetch from test component

--- frontend/src/app/list/list.component.spec.ts
+++ frontend/src/app/list/list.component.spec.ts
@@ -2,10 +2,6 @@ describe('Async component', ()=>
 {
 	test('render list items with mocks', async() =>
 	{
-		window.fetch = jest.fn().mockResolvedValueOnce({
-			json: async() =>[ { id:'1', title:'Post from Jest' } ]
-		});
-
 		expect(true).toBe(true);
 	});
 });
