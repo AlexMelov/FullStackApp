2380324f02b2935ce19645ee0cae2b77cff89288
Aleksandar <melov.aleksandar2@gmail.com>
2022-04-19 11:19:24 +0200

    adding form to the code

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,45 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
 import { FormComponent } from './todo/form/form.component';
 import { ListComponent } from './todo/list/list.component';
-import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatListModule } from '@angular/material/list';
-import { MatDividerModule } from '@angular/material/divider';
+import { MatNativeDateModule } from '@angular/material/core';
+import { Material } from '../material.module';
 
 @NgModule(
-{
-	declarations:
-	[
-		AppComponent,
-		TodoComponent,
-		FormComponent,
-		ListComponent
-	],
-	imports:
-	[
-		BrowserModule,
-		FormsModule,
-		HttpClientModule,
-		CommonModule,
-		BrowserAnimationsModule,
-		MatButtonModule,
-		MatListModule,
-		MatDividerModule
-	],
-	providers:
-	[],
-	bootstrap:
-	[
-		AppComponent
-	]
-})
+	{
+		declarations:
+			[
+				AppComponent,
+				TodoComponent,
+				FormComponent,
+				ListComponent
+			],
+		imports:
+			[
+				BrowserAnimationsModule,
+				BrowserModule,
+				FormsModule,
+				HttpClientModule,
+				MatNativeDateModule,
+				ReactiveFormsModule,
+				Material
+			],
+		providers:
+			[],
+		bootstrap:
+			[
+				AppComponent
+			]
+	})
 export class AppModule
 {
 }
