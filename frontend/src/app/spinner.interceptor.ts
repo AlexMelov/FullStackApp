d3b215195affd22ad7f13bcbd6abeea2e180bfdb
Aleksandar <melov.aleksandar2@gmail.com>
2022-04-26 21:59:26 +0200

    rename listService into spinnerStore

--- frontend/src/app/spinner.interceptor.ts
+++ frontend/src/app/spinner.interceptor.ts
@@ -12,16 +12,12 @@ import { SpinnerStore } from './todo/list/spinner.store';
 @Injectable()
 export class SpinnerInterceptor implements HttpInterceptor
 {
-	constructor(private listService : SpinnerStore)
+	constructor(private spinnerStore : SpinnerStore)
 	{}
 
 	intercept(request : HttpRequest<unknown>, next : HttpHandler) : Observable<HttpEvent<unknown>>
 	{
-  	this.listService.show();
-  	return next.handle(request).pipe(
-		finalize(() =>
-		{
-			this.listService.hide();
-		}));
+  	this.spinnerStore.show();
+  	return next.handle(request).pipe(finalize(() =>this.spinnerStore.hide()));
 	}
 }
