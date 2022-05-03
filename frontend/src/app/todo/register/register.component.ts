13f5f81a499ce6138ccebab1681aeb15ba57c412
Aleksandar <melov.aleksandar2@gmail.com>
2022-05-03 11:57:57 +0200

    Output credentials

--- frontend/src/app/todo/register/register.component.ts
+++ frontend/src/app/todo/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component(
 	{
@@ -10,5 +10,21 @@ import { Component } from '@angular/core';
 
 export class RegisterComponent
 {
+	@Output() onRegister : EventEmitter<object> = new EventEmitter<object>();
+
 	hide : boolean = true;
+	user_email : string | null = null;
+	user_password : string | null = null;
+
+	userCredentials() : void
+	{
+		this.onRegister.emit(
+		{
+			email: this.user_email,
+			password: this.user_password
+		}
+		);
+	this.user_password = null;
+	this.user_email = null;
+	}
 }
