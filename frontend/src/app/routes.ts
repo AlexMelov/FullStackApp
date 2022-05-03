f64a3eb921f8fbdbbe381a458ff399e640995ba0
Aleksandar <melov.aleksandar2@gmail.com>
2022-05-03 17:01:53 +0200

    remove husky pre-commit to auto fix

--- frontend/src/app/routes.ts
+++ frontend/src/app/routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { TodoComponent } from './todo/todo.component';
-import { AuthenticationComponent } from "./authentication/authentication.component";
+import { AuthenticationComponent } from './authentication/authentication.component';
 
 export const routes : Routes =
 	[
