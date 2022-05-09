0dbac0c6f90c3c4ed4d513fc52302f1669fa44f6
Aleksandar <melov.aleksandar2@gmail.com>
2022-05-09 11:32:10 +0200

    add router guard

--- frontend/src/app/authentication/login/login.component.ts
+++ frontend/src/app/authentication/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LoginService } from './login.service';
+import { Router } from '@angular/router';
+import { environment } from '../../../environments/environment';
 
 @Component(
 {
@@ -14,7 +16,7 @@ export class LoginComponent
 	form : FormGroup;
 	private token : string | null = null;
 
-	constructor(private formBuilder : FormBuilder, private loginServices : LoginService)
+	constructor(private formBuilder : FormBuilder, private loginServices : LoginService, private router : Router)
 	{
 		this.form = this.createForm();
 	}
@@ -27,6 +29,7 @@ export class LoginComponent
 			.subscribe(response =>
 			{
 				this.token = response.token;
+				this.router.navigate([ environment.pageRoutes.todos ]);
 			});
 	}
 
