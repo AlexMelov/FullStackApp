f3e9f17b7e8b43a366a9b4f2edccb86e31b9d045
henryruhs <info@henryruhs.com>
2022-05-10 11:03:08 +0200

    Improve auth promise handling

--- backend/src/controlers/authentication.ts
+++ backend/src/controlers/authentication.ts
@@ -1,41 +1,38 @@
 import { Request, Response } from 'express';
 import { User } from '../models/user.interface';
-import { compare } from 'bcrypt';
+import { compareSync } from 'bcrypt';
 import { Model } from 'mongoose';
 import { tokenHelper } from './token.helper.js';
 
 export function loginUserHandler(request : Request, response : Response, userModel : Model<User>) : void
 {
 	const { email, password } = request.body;
-	//todo change userData into usee
-	let userData : User & {_id : string | {}};
 
 	userModel.findOne(
 	{
 		email
 	})
-		//todo put everything in one then unify user and user data
-		.then(user =>
+	.then(user =>
+	{
+		return { compare: compareSync(password, user.password), user };
+	})
+	.then(result =>
+	{
+		if(result.compare)
 		{
-			userData = user;
-			return compare(password, user.password);
-		})
-		.then(result =>
-		{
-			if(!result)
+			return response.status(200).json(
 			{
-				return response.status(401).json(
-				{
-					//todo add message:  from wording file
-					message: 'Authentication failed on user'
-				});
-			}
-			response.status(200).json(
-			{
-				token : tokenHelper(userData)
+				token: tokenHelper(result.user)
 			});
-		})
-		//todo add message:  from wording file
-		.catch(error => response.status(401)
-			.json({ message: 'Authentication failed on entire', error }));
+		}
+		return response.status(401).json(
+		{
+			//todo add message:  from wording file
+			message: 'Authentication failed on user'
+		});
+
+	})
+	//todo add message:  from wording file
+	.catch(error => response.status(401)
+		.json({ message: 'Authentication failed on entire', error }));
 }
