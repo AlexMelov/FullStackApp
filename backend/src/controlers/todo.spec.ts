5c3f6e68a5bcbc481ed2da4e499573e2326b2277
Aleksandar <melov.aleksandar2@gmail.com>
2022-05-10 11:17:32 +0200

    fixed jest tests on backend and todos tests moved into todo.spec.ts from server.spec.ts

---
+++ backend/src/controlers/todo.spec.ts
@@ -0,0 +1,82 @@
+import supertest, { Response } from 'supertest';
+import mongoose from 'mongoose';
+import { server } from '../server';
+import { DirtyTodo, TestTodo } from '../models/todo';
+
+describe('Todos', () =>
+{
+	beforeAll(async() => await mongoose.connect(process.env.DB_URL));
+
+	it('should GET all todos', async() =>
+	{
+		const response : Response = await supertest(server).get('/todos');
+		const body : DirtyTodo[] = await response.body;
+
+		expect(response.statusCode).toBe(200);
+		expect(body).not.toHaveLength(0);
+		expect(body.forEach(todo =>
+		{
+			expect(todo.title).toBeTruthy();
+		}));
+	});
+
+	it('should CREATE a todo', async() =>
+	{
+		const response : Response = await supertest(server).post('/todos').send(
+			{
+				title : 'Todo from Jest!'
+			});
+		const body : DirtyTodo = await response.body;
+
+		expect(response.statusCode).toBe(200);
+		expect(response.headers).toBeDefined();
+		expect(body.title).toContain('Todo from Jest!');
+	});
+
+	it('Should GET last added todo from list and delete it', async() =>
+	{
+		const response : Response = await supertest(server).get('/todos');
+		const body : TestTodo[] = await response.body;
+		const todoArray : TestTodo[] = await body.filter(todo => todo.title === 'Todo from Jest!' );
+
+		expect(response.statusCode).toBe(200);
+		expect(body).not.toHaveLength(0);
+		expect(body.forEach(todo =>
+		{
+			expect(todo.title).toBeTruthy();
+		}));
+		await supertest(server).delete('/todos/' + todoArray[0].id).then(user =>
+		{
+			expect(user.statusCode).toBe(200);
+			expect(user.body).toEqual(
+				{
+					acknowledged: true,
+					deletedCount: 1
+				});
+		});
+	});
+
+	it('should create and DELETE same todo', async() =>
+	{
+		const response : Response = await supertest(server).post('/todos').send(
+			{
+				title : 'Todo for delete from Jest!'
+			});
+		const body : DirtyTodo = await response.body;
+
+		expect(response.statusCode).toBe(200);
+		expect(response.headers).toBeDefined();
+		expect(body.title).toContain('Todo for delete from Jest!');
+
+		const deleteResponse : Response = await supertest(server).delete('/todos/' + body._id);
+
+		expect(deleteResponse.statusCode).toBe(200);
+		expect(deleteResponse.body).toEqual(
+			{
+				acknowledged: true,
+				deletedCount: 1
+			});
+	});
+
+	afterAll(() => mongoose.connection.close(true));
+});
--- backend/src/server.spec.ts
+++ backend/src/server.spec.ts
@@ -1,8 +1,6 @@
 import supertest, { Response } from 'supertest';
 import mongoose from 'mongoose';
 import { server } from './server.js';
-import { Body } from './server.interface.js';
-import { TestTodo } from './models/todo';
 
 describe('Server', () =>
 {
@@ -13,77 +11,6 @@ describe('Server', () =>
 		const response : Response = await supertest(server).get('/');
 
 		expect(response.statusCode).toBe(404);
-	});
-
-	it('should GET all todos', async() =>
-	{
-		const response : Response = await supertest(server).get('/todos');
-		const body : Body[] = await response.body;
-
-		expect(response.statusCode).toBe(200);
-		expect(body).not.toHaveLength(0);
-		expect(body.forEach(todo =>
-		{
-			expect(todo.title).toBeTruthy();
-		}));
-	});
-
-	it('should CREATE a todo', async() =>
-	{
-		const response : Response = await supertest(server).post('/todos').send(
-		{
-			title : 'Todo from Jest!'
-		});
-		const body : Body = await response.body;
-
-		expect(response.statusCode).toBe(200);
-		expect(response.headers).toBeDefined();
-		expect(body.title).toContain('Todo from Jest!');
-	});
-
-	it('Should GET last added todo from list and delete it', async() =>
-	{
-		const response : Response = await supertest(server).get('/todos');
-		const body : TestTodo[] = await response.body;
-		const todoArray : TestTodo[] = await body.filter(todo => todo.title === 'Todo from Jest!' );
-
-		expect(response.statusCode).toBe(200);
-		expect(body).not.toHaveLength(0);
-		expect(body.forEach(todo =>
-		{
-			expect(todo.title).toBeTruthy();
-		}));
-		await supertest(server).delete('/todos/' + todoArray[0].id).then(user =>
-		{
-			expect(user.statusCode).toBe(200);
-			expect(user.body).toEqual(
-			{
-				acknowledged: true,
-				deletedCount: 1
-			});
-		});
-	});
-
-	it('should create and DELETE same todo', async() =>
-	{
-		const response : Response = await supertest(server).post('/todos').send(
-		{
-			title : 'Todo for delete from Jest!'
-		});
-		const body : Body = await response.body;
-
-		expect(response.statusCode).toBe(200);
-		expect(response.headers).toBeDefined();
-		expect(body.title).toContain('Todo for delete from Jest!');
-
-		const deleteResponse : Response = await supertest(server).delete('/todos/' + body._id);
-
-		expect(deleteResponse.statusCode).toBe(200);
-		expect(deleteResponse.body).toEqual(
-		{
-			acknowledged: true,
-			deletedCount: 1
-		});
 	});
 
 	afterAll(() => mongoose.connection.close(true));
