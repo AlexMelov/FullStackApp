c9c629c8cb91693028c70243dd7b8d3506047059
Aleksandar Melov <80846978+AlexMelov@users.noreply.github.com>
2022-05-11 10:57:39 +0200

    Fixing deploy (#89)
     fixing deploy routes (hardcoded)

--- frontend/src/app/routes.ts
+++ frontend/src/app/routes.ts
@@ -3,9 +3,14 @@ import { TodoComponent } from './todo/todo.component';
 import { environment } from '../environments/environment';
 import { RegisterComponent } from './authentication/register/register.component';
 import { LoginComponent } from './authentication/login/login.component';
+import { HeaderComponent } from "./header/header.component";
 
 export const routes : Routes =
 [
+	{
+		path: '',
+		component: HeaderComponent
+	},
 	{
 		path: environment.pageRoutes.todos,
 		component: TodoComponent
