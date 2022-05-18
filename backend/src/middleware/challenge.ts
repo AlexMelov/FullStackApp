43f633625b200460f5b65e80db7b72ae6278950b
henryruhs <info@henryruhs.com>
2022-05-18 16:55:17 +0200

    Fix challenge middleware

--- backend/src/middleware/challenge.ts
+++ backend/src/middleware/challenge.ts
@@ -3,46 +3,38 @@ import { sendLoginMail } from '../controllers/mailer.js';
 import { userModel } from '../models/user.schema.js';
 import { compareSync } from 'bcrypt';
 
-export const store : Map<string, number> = new Map();
-export let generatedChallenge : number = 0;
+const store : Map<string, number> = new Map();
 
 export function challengeMiddleware(request : Request, response : Response, next : NextFunction) : void
 {
 	const { email, challenge, password } = request.body;
 
 	userModel.findOne(
+	{
+		email
+	})
+	.then(user =>
+	{
+		return { compare: compareSync(password, user.password), user };
+	}).then(result =>
+	{
+		if (result.compare && !challenge)
 		{
-			email
-		})
-		.then(user =>
-		{
-			return { compare: compareSync(password, user.password), user };
-		}).then(result =>
-		{
-			if (result.compare)
-			{
-				if (!challenge)
-				{
-					const createdChallenge : number = createChallenge();
+			const createdChallenge : number = createChallenge();
 
-					store.set(email, createdChallenge);
-					sendLoginMail(email, createdChallenge);
-					generatedChallenge = createdChallenge;
+			store.set(email, createdChallenge);
+			sendLoginMail(email, createdChallenge);
 
-					response.status(200).json(
-					{
-						action: 'request-challenge'
-					});
-				}
-				else
+			response.status(200).json(
 				{
-					if (challenge === generatedChallenge.toString())
-					{
-						next();
-					}
-				}
-			}
-		}).catch((error : Error) => response.status(401).json(error.message));
+					action: 'request-challenge'
+				});
+		}
+		else if (store.get('email') === challenge)
+		{
+			next();
+		}
+	}).catch((error : Error) => response.status(401).json(error.message));
 }
 
 function createChallenge() : number
