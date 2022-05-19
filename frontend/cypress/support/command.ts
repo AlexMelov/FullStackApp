e16a3343b5ffa0727d87fe754b4668365869028c
Aleksandar <melov.aleksandar2@gmail.com>
2022-05-19 10:53:59 +0200

    remove wait from cypress test

--- frontend/cypress/support/command.ts
+++ frontend/cypress/support/command.ts
@@ -10,12 +10,10 @@ Cypress.Commands.add('login', () =>
 	cy.get('[data-test="login-password"]').type('123456789');
 	cy.get('[data-test="login-button"]').click();
 	cy.get('[data-test="login-challenge"]').type('1234');
-	cy.wait(2000);
 });
 
 Cypress.Commands.add('logout', () =>
 {
 	cy.visit(environment.baseUrl + '/' + environment.pageRoutes.login);
 	cy.get('[data-test="logout-button"]').click();
-	cy.wait(2000);
 });
